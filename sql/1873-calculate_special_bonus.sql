cd2fdae82ec84994f065f6c8e4fd947f6b39377e
Robert812218 <robert812@shaw.ca>
2023-03-29 10:02:10 -0600

    another 3 SQL

--- sql/1873-calculate_special_bonus.sql
+++ sql/1873-calculate_special_bonus.sql
@@ -1,7 +1,3 @@
-SELECT employee_id, 
-       CASE 
-           WHEN employee_id % 2 <> 0 AND name NOT LIKE 'M%' THEN salary 
-           ELSE 0 
-       END AS bonus 
-FROM Employees 
-ORDER BY employee_id;
+select employee_id, 
+if(employee_id % 2 = 0 OR name LIKE 'M%', 0, salary) as bonus 
+from Employees;
